2cc7ebce0ac674e942215b038a8bce00f1736096
trixterx101-web <trixterx101@gmail.com>
2026-04-29 21:15:34 +0300

    fix: revert youtube scope to fix RefreshError

--- src/uploader.py
+++ src/uploader.py
@@ -8,8 +8,7 @@ class YouTubeUploader:
     def __init__(self, client_secrets_file):
         # youtube.force-ssl is REQUIRED for thumbnails().set()
         self.scopes = [
-            "https://www.googleapis.com/auth/youtube.upload",
-            "https://www.googleapis.com/auth/youtube.force-ssl"
+            "https://www.googleapis.com/auth/youtube.upload"
         ]
         self.client_secrets_file = client_secrets_file
         self.youtube = self.get_authenticated_service()
